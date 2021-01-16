7d5d986b0e108fcef49be8e0f4413d83e9a6bbca
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-01-16 14:13:07 -0500

    Updated Accel Data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,57 +1,94 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
-import { firebase } from "../config";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
-export default function HomeScreen({ navigation }) {
-  const updateCoordinates = () => {
-    let newCoordinateKey = firebase.database().ref().child("coordinates").push()
-      .key;
+export default function App() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
 
-    let coordinate_data = {
-      Longitude: 1,
-      Latitude: 1,
-      Time: Math.floor(Date.now() / 1000),
-    };
-
-    let updates = {};
-    updates["coordinates/" + newCoordinateKey] = coordinate_data;
-
-    try {
-      firebase.database().ref().update(updates);
-    } catch (error) {
-      alert(error);
-    }
+  const _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
   };
 
-  const getCoordinates = () => {
-    let coordinateRef = firebase.database().ref("coordinates");
-    let index = 0;
-    coordinateRef.on("value", (snapshot) => {
-      snapshot.forEach((child1) => {
-        child1.forEach((child2) => {
-          if (index % 3 == 2) {
-            console.log(child2.val());
-          }
-          index++;
-        });
-      });
-    });
+  const _fast = () => {
+    Accelerometer.setUpdateInterval(16);
   };
 
+  const _subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const { x, y, z } = data;
   return (
-    <View>
-      <Text
-        onPress={updateCoordinates}
-        style={{ marginLeft: 100, marginTop: 100 }}
-      >
-        Text
+    <View style={styles.container}>
+      <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+      <Text style={styles.text}>
+        x: {round(x)} y: {round(y)} z: {round(z)}
       </Text>
-      <Text
-        onPress={getCoordinates}
-        style={{ marginLeft: 100, marginTop: 200 }}
-      >
-        Cool
-      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+          <Text>{subscription ? 'On' : 'Off'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+  return Math.floor(n * 100) / 100;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  text: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+});
