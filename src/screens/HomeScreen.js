3b4670e75bac2c672fbe03932ba60bf9f9b61d38
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-01-16 19:12:40 -0500

    Stores location of potholes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,50 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Platform} from 'react-native';
-import * as Location from 'expo-location';
-import { Accelerometer } from 'expo-sensors';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as Location from "expo-location";
+import { Accelerometer } from "expo-sensors";
+import { firebase } from "../config";
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [timestamp, setTimestamp] = useState(null);
   const [data, setData] = useState({
     x: 0,
     y: 0,
     z: 0,
   });
+  const [previousData, setPreviousData] = useState(0);
   const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let loc = await Location.watchPositionAsync(
+        {
+          timeInterval: 0,
+          accuracy: Location.Accuracy.BestForNavigation,
+        },
+        (loc) => updateHookdata(loc)
+      );
+    })();
+  }, []);
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const updateHookdata = (loc) => {
+    setLatitude(JSON.stringify(loc.coords.latitude));
+    setLongitude(JSON.stringify(loc.coords.longitude));
+    setTimestamp(JSON.stringify(loc.timestamp));
+  };
+
+  const updateCoordinates = () => {
+    let newCoordinateKey = firebase.database().ref().child("coordinates").push()
+      .key;
+
+    let coordinate_data = {
+      Longitude: longitude,
+      Latitude: latitude,
+      Time: timestamp,
+    };
+
+    let updates = {};
+    updates["coordinates/" + newCoordinateKey] = coordinate_data;
+
+    try {
+      firebase.database().ref().update(updates);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   const _slow = () => {
     Accelerometer.setUpdateInterval(1000);
   };
 
   const _fast = () => {
-    Accelerometer.setUpdateInterval(16);
+    Accelerometer.setUpdateInterval(100);
   };
 
   const _subscribe = () => {
     setSubscription(
-      Accelerometer.addListener(accelerometerData => {
+      Accelerometer.addListener((accelerometerData) => {
         setData(accelerometerData);
       })
     );
@@ -55,26 +88,38 @@ export default function App() {
     setSubscription(null);
   };
 
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
-
+  if (
+    data.y > 1.5 &&
+    previousData != timestamp &&
+    latitude &&
+    longitude &&
+    timestamp
+  ) {
+    setPreviousData(timestamp);
+    updateCoordinates();
+  }
   const { x, y, z } = data;
+
   return (
-
-
     <View style={styles.container}>
-      <Text style={styles.text}>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+      <Text style={styles.text}>
+        Accelerometer: (in Gs where 1 G = 9.81 m s^-2)
+      </Text>
       <Text style={styles.text}>
         x: {round(x)} y: {round(y)} z: {round(z)}
       </Text>
-      <Text style={styles.paragraph}>{text}</Text>
+      <Text style={styles.paragraph}>{timestamp}</Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
+        <TouchableOpacity
+          onPress={subscription ? _unsubscribe : _subscribe}
+          style={styles.button}
+        >
+          <Text>{subscription ? "On" : "Off"}</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
           <Text>Slow</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={_fast} style={styles.button}>
@@ -95,27 +140,27 @@ function round(n) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 10,
   },
   text: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'stretch',
+    flexDirection: "row",
+    alignItems: "stretch",
     marginTop: 15,
   },
   button: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
     padding: 10,
   },
   middleButton: {
     borderLeftWidth: 1,
     borderRightWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
   },
 });
